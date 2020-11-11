db684f133883ea586db2869434d3056e7b016ffb
Gabriel Guimarães Paixão <72279737+GabrielPaixaoo@users.noreply.github.com>
2020-11-11 16:53:26 -0300

    Update Caixa.java

--- Restaurante/Caixa.java
+++ Restaurante/Caixa.java
@@ -1,3 +1,26 @@
-public class Caixa {
-    
+public class Caixa{
+
+    private float saida;
+    private float comissao;
+    private float total; // dinheiro total no caixa
+    private Conta conta;
+
+    public Caixa(Conta conta){
+        this.conta = conta;
+    }
+
+    void setTotal(float x){
+        this.total += x;
+    }
+    void setSaida(float x){
+        this.total -= x;
+        this.saida += x;
+    }
+    float getTotal(){
+        return this.total;
+    }
+    float getSaida(){
+        return this.saida;
+    }
+
 }
